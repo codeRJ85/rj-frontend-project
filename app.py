1a045cd6b5131eaec9a42eed7e19576f7b92b22a
Raja Jha <126911946+codeRJ85@users.noreply.github.com>
2023-03-06 10:07:06 +0000

    added jobs

--- app.py
+++ app.py
@@ -1,10 +1,43 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Data Analyst',
+        'location': 'Bangaluru,India',
+        'salary': 'Rs. 1,00,000'
+    },
+    {
+        'id': 2,
+        'title': 'Video Editor',
+        'location': 'Delhi,India',
+        'salary': 'Rs. 5,00,000'
+    },
+    {
+        'id': 3,
+        'title': 'Data Scientist',
+        'location': 'Kolkata,India',
+        'salary': 'Rs. 3,00,000'
+    },
+    {
+        'id': 4,
+        'title': 'Front-end Developer',
+        'location': 'Bangaluru,India',
+        'salary': 'Rs. 4,00,000'
+    },
+    {
+        'id': 5,
+        'title': 'Backend Developer',
+        'location': 'Chennai,India',
+        'salary': 'Rs. 6,00,000'
+    }
+]
+
 @app.route("/")
 def hello_world():
-  return render_template("home.html")
+    return render_template('home.html', jobs=JOBS, company_name='Rcareer')
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0',debug=True)
+    app.run(host='0.0.0.0', debug=True)
